ec0155f2581c3a081fe3b37de14147836812f7d1
AzerothA <telifesite@gmail.com>
2023-12-08 22:08:46 +0330

    --print-wat flag added to compile commend

--- figcli/src/main.rs
+++ figcli/src/main.rs
@@ -1,12 +1,13 @@
+use clap::{Args, Parser, Subcommand};
+use figc::codegen::codegen::Context;
+use figc::lexer::lexer::Lexer;
+use figc::parser::ast::Program;
+use figc::parser::parser::Parser as FigParser;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::PathBuf;
 use std::process::exit;
-use clap::{Parser, Subcommand, Args};
-use figc::{lexer::lexer::Lexer, };
-use figc::codegen::codegen::Context;
-use figc::parser::ast::Program;
-use figc::parser::{parser::Parse, parser::Parser as FigParser};
+use wasmprinter::print_bytes;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -25,6 +26,10 @@ enum Commands {
 struct CompileArgs {
     /// Fig file path
     fig_file_path: PathBuf,
+
+    #[arg(long, action)]
+    /// Print Wat
+    print_wat: bool,
 }
 
 /// Expects SourceCode returns Wasm binary
@@ -33,10 +38,9 @@ fn fig_compile_to_wasm(source: String, memory_offset: i32) -> Vec<u8> {
     let mut parser = FigParser::new(&mut lexer);
     let program = Program::parse(&mut parser, None);
 
-
-        for error in program.get_errors(){
-            println!("{}", error);
-        }
+    for error in program.get_errors() {
+        println!("{}", error);
+    }
 
     let mut ctx = Context::new(program, memory_offset);
     ctx.bootstrap();
@@ -52,7 +56,7 @@ fn fig_compile_to_wasm(source: String, memory_offset: i32) -> Vec<u8> {
     buf
 }
 
-fn main () {
+fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
@@ -68,11 +72,18 @@ fn main () {
             // Now compile
             let final_wasm = fig_compile_to_wasm(buffer, 0x0);
 
+            if c_args.print_wat {
+                let wasm = print_bytes(&final_wasm)
+                    .unwrap_or("Could't print compiled wasm to wat!".to_string());
+
+                println!("{}", wasm);
+            };
+
             // Create A Compiled wasm file
-            let wasm_file_path =
-                PathBuf::from(
-                    format!("./{}.wasm", c_args.fig_file_path.file_stem().unwrap().to_str().unwrap())
-                );
+            let wasm_file_path = PathBuf::from(format!(
+                "./{}.wasm",
+                c_args.fig_file_path.file_stem().unwrap().to_str().unwrap()
+            ));
 
             let mut wasm_file = File::create(wasm_file_path).unwrap();
             wasm_file.write_all(&*final_wasm).unwrap();
